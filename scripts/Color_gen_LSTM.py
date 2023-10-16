c2deb9a84827bca5da1d6b019013b40516fa384e
Aarush Sinha <90822297+chungimungi@users.noreply.github.com>
2023-10-17 02:19:13 +0530

    Update Color_gen_LSTM.py

--- scripts/Color_gen_LSTM.py
+++ scripts/Color_gen_LSTM.py
@@ -1,18 +1,25 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Bidirectional, SimpleRNN, GRU, LSTM
+from tensorflow.keras.layers import Dense, Dropout, Bidirectional, LSTM
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing import sequence
 import numpy as np
 import pandas as pd
-from keras.utils import to_categorical
-import matplotlib.pyplot as plt
 
+if tf.test.is_gpu_available():
+    # Force TensorFlow to use the GPU
+    config = tf.compat.v1.ConfigProto(device_count={'GPU': 1})
+    config.gpu_options.allow_growth = True  # Allow GPU memory growth
+    tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+else:
+    print("No GPU available, using CPU.")
+
+# Load and preprocess your data
 np.random.seed(10)
-data = pd.read_csv('colors.csv')
+data = pd.read_csv("colors.csv")
 names = data["name"]
 
-maxlen = 25
+maxlen = 100
 t = Tokenizer(char_level=True)
 t.fit_on_texts(names)
 tokenized = t.texts_to_sequences(names)
@@ -24,8 +31,10 @@ def scale(n):
 normalized_values = np.column_stack([data["red"], data["green"], data["blue"]])
 normalized_values = normalized_values / 255.0
 
+# Create and compile your model
 model = Sequential()
-model.add(Bidirectional(LSTM(256, activation='tanh'), input_shape=(maxlen, 3)))
+model.add(Conv1D(256, kernel_size=3, activation='relu', input_shape=(maxlen, 3)))
+model.add(Bidirectional(LSTM(256, activation='tanh')))
 model.add(Dense(128, activation='tanh'))
 model.add(Dense(128, activation='tanh'))
 model.add(Dense(64, activation='tanh'))
@@ -33,6 +42,47 @@ model.add(Dense(64, activation='tanh'))
 model.add(Dense(3, activation='sigmoid'))
 model.add(Dropout(0.2))
 model.compile(optimizer='adam', loss='mse', metrics=['acc'])
+
+# Train the model with the initial data
+history = model.fit(np.repeat(padded_names[:, :, np.newaxis], 3, axis=-1), normalized_values, epochs=350, batch_size=512, validation_split=0.20)
+
+# Function to generate color names by combining adjectives and colors
+def generate_color_names():
+    with open('adj.txt', 'r') as file:
+        adjectives = file.read().splitlines()
+
+    colors = ["Red", "Yellow", "Blue", "Orange", "Green", "Violet"]
+
+    for adjective in adjectives:
+        for color in colors:
+            yield f"{adjective} {color}"
+
+path = 'colors.csv'
+
+# Function to predict RGB values and update the CSV
+def predict_and_update(name):
+    global path
+    name = name.lower()
+    tokenized = t.texts_to_sequences([name])
+    padded = sequence.pad_sequences(tokenized, maxlen=maxlen)
+    pred = model.predict(np.repeat(padded[:, :, np.newaxis], 3, axis=-1))[0]
+    r, g, b = scale(pred[0]), scale(pred[1]), scale(pred[2])
+    data.loc[len(data)] = [name, r, g, b]  # Add the new RGB values to the CSV
+    path = 'colors.csv'
+    data.to_csv(path, index=False)
+
+# Automated loop for generating and updating color names
+for color_name in generate_color_names():
+    predict_and_update(color_name)
+
+print("\n Task Done \n")
+
+# Retrain the model after adding new data
+names = data["name"]
+normalized_values = np.column_stack([data["red"], data["green"], data["blue"]])
+normalized_values = normalized_values / 255.0
+tokenized = t.texts_to_sequences(names)
+padded_names = sequence.pad_sequences(tokenized, maxlen=maxlen)
 
 history = model.fit(np.repeat(padded_names[:, :, np.newaxis], 3, axis=-1), normalized_values, epochs=350, batch_size=256, validation_split=0.20)
 
@@ -50,19 +100,6 @@ def predict(name):
     r, g, b = scale(pred[0]), scale(pred[1]), scale(pred[2])
     print(name + ',', 'R,G,B:', r, g, b)
     plot_rgb(pred)
-
-model.summary()
-
-train_acc = history.history['acc']
-val_acc = history.history['val_acc']
-
-plt.plot(train_acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
 
 n = input()
 predict(n)
